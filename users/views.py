a71f718ee4217f732abfd3290485e226e08acb11
esergievich <sergievichevgeniy@gmail.com>
2025-04-27 01:39:30 +0300

    feat: create RegisterUser view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import RegisterUserForm
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': "Регистрация"}
+    success_url = reverse_lazy('users:login')
