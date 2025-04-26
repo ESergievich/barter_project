aa321af565b552bab7adb288462cd05bfb906aac
esergievich <sergievichevgeniy@gmail.com>
2025-04-27 01:38:48 +0300

    feat: create RegisterUserForm

---
+++ users/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label="Логин")
+    password1 = forms.CharField(label="Пароль")
+    password2 = forms.CharField(label="Повтор пароля")
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        labels = {
+            'email': 'E-mail',
+            'first_name': "Имя",
+            'last_name': "Фамилия",
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError("Такой E-mail уже существует!")
+        return email
